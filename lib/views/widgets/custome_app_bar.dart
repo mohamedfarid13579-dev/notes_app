94de950398c29168b7d39c15d2de64159f62b6b5
mohamed-farid <59448168+mohamed-farid@users.noreply.github.com>
2026-02-01 00:05:10 +0200

    add custome app bar to edit notes view

--- lib/views/widgets/custome_app_bar.dart
+++ lib/views/widgets/custome_app_bar.dart
@@ -2,15 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custome_search_icon.dart';
 
 class CustomeAppBar extends StatelessWidget {
-  const CustomeAppBar({super.key});
+  const CustomeAppBar({super.key, required this.title, required this.icon});
 
+  final String title;
+  final IconData icon;
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
       children: [
-        Text('notes', style: TextStyle(fontSize: 28, color: Colors.white)),
+        Text(title, style: TextStyle(fontSize: 28, color: Colors.white)),
         Spacer(),
-        CustomeSearchIcon(),
+        CustomeSearchIcon(
+          icon: icon,
+        ),
       ],
     );
   }
--- lib/views/widgets/edit_note_view_body.dart
+++ lib/views/widgets/edit_note_view_body.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custome_app_bar.dart';
-import 'package:notes_app/views/widgets/notes_list_view.dart';
 
-class NotesViewBody extends StatelessWidget {
-  const NotesViewBody({super.key});
+class EditNoteViewBody extends StatelessWidget {
+  const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsetsGeometry.symmetric(horizontal: 24),
+      padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         children: [
           SizedBox(height: 50),
           CustomeAppBar(
-            title: 'Notes',
-            icon: Icons.search,
+            title: 'Edit Notes',
+            icon: Icons.check,
           ),
-          Expanded(child: NotesListView()),
         ],
       ),
     );
